7e4f6d45dae92f217ba00b0e16f9ccae6e1de0cb
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-04-28 14:33:45 +0100

    🎨 Cleanup of assembler directives and added template

--- starter.asm
+++ starter.asm
@@ -1,8 +1,8 @@
 ;##############################################################################
-; Assembly program that clears the page zero region (0x00-0xFF)
 ; 
-; Page Zero is the first 256 bytes of the 6502 memory map, 
-; covering 128 bytes of RAM and 128 bytes of TIA registers
+; 
+; 
+; 
 ;##############################################################################
 
     .processor 6502          ; 6502 processor
@@ -16,17 +16,9 @@ Start:
     txs                     ; Transfer X to stack pointer register
 
 ;##############################################################################
-; Clear the page zero region (0x00-0xFF)
-; This means the entire RAM + TIA space
+; 
+; 
 ;##############################################################################
-    lda #0                  ; A = 0x00
-    ldx #$FF                ; X = 0xFF
-    sta $FF                 ; Store A (0) at 0xFF (as loop will start at FE)
-
-ClearPageZero:
-    dex                     ; X--
-    sta $0,X                ; Store A at 0x00 + X => NO SPACE after comma!   
-    bne ClearPageZero       ; Loop until X = 0x00 (=> z flag is set)   
 
 ;##############################################################################
 ; Cleanup
