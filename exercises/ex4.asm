ca8bfacee75a2e7fdf8634103c3a61fba9a503ba
Janos Jarecsni <janos.jarecsni@gmail.com>
2024-05-03 12:51:22 +0100

    🚑 Call CLC before ADC :)

--- exercises/ex4.asm
+++ exercises/ex4.asm
@@ -17,6 +17,7 @@ Start:
 ; Main program
 ;##############################################################################
     lda #100                ; Load the A register with the literal decimal value 100
+    clc                     ; Clear the carry flag
     adc #5                  ; Add the decimal value 5 to the accumulator
     sbc #10                 ; Subtract the decimal value 10 from the accumulator
                             ; Register A should now contain the decimal 95 (or $5F in hexadecimal)
