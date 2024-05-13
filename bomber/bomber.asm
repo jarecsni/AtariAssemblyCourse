b6c77a1bdb09c29a6532315eea3dd53113f0b570
Janos Jarecsni <janos.jarecsni@gmail.com>
2024-05-13 23:44:04 +0100

    ✨ Blue screen :)

--- bomber/bomber.asm
+++ bomber/bomber.asm
@@ -31,8 +31,64 @@ Reset:
     CLEAN_START             ; Clean start (macro.h)
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Render frame
+;; Initialise RAM variables and TIA registers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #10                 ; 
+    sta JetYPos             ; JetYPos = 10
+    lda #60                 ;
+    sta JetXPos             ; JetXPos = 60
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Main Display Loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+StartFrame:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; VBLANK & VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #2
+    sta VBLANK             ; VBLANK on
+    sta VSYNC              ; VSYNC on
+    .repeat 3
+        sta WSYNC          ; Wait for sync
+    .repend
+    lda #0
+    sta VSYNC              ; VSYNC off
+    .repeat 37
+        sta WSYNC          ; Wait for sync
+    .repend
+    sta VBLANK             ; VBLANK off
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display 192 visible lines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+VisibleLines:
+    lda #$84                ; NTSC color blue
+    sta COLUBK              ; Background color
+    lda #$C2                ; NTSC color green
+    sta COLUPF              ; Playfield color
+
+    ldx #192                ; x = 192 (visible lines)
+.GameLineLoop:
+    sta WSYNC               ; Wait for sync
+    dex                     ; x--
+    bne .GameLineLoop       ; Loop until x = 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Overscan
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #2                  ;   
+    sta VBLANK              ; VBLANK on
+    .repeat 30
+        sta WSYNC           ; Wait for sync
+    .repend
+    lda #0                  ;
+    sta VBLANK              ; VBLANK off
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; End of Display Loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    jmp StartFrame          ; Jump to the start of the display loop
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Cleanup
